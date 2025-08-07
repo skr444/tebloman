5e158ccdd492a634e2fea3ef6e00fb23cfeca212
skr444 <15791510+skr444@users.noreply.github.com>
2025-08-07 12:12:57 +0200

    wip: support wpf native can execute changed

---
+++ src/Skr.Tebloman.Ui/Helper/WpfRelayCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.Input;
+
+namespace Skr.Tebloman.Ui.Helper
+{
+    /// <summary>
+    /// WPF friendly implementation of <see cref="RelayCommand"/>.
+    /// </summary>
+    /// <remarks>
+    ///     Uses the <see cref="CommandManager"/> to propagate and update can execute states.
+    /// </remarks>
+    public sealed class WpfRelayCommand : IRelayCommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool>? canExecute;
+
+        /// <inheritdoc/>
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="WpfRelayCommand"/>.
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public WpfRelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <inheritdoc/>
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute?.Invoke() ?? true;
+        }
+
+        /// <inheritdoc/>
+        public void Execute(object? parameter)
+        {
+            execute();
+        }
+
+        /// <summary>
+        /// Does nothing since can execute changed handling is done through <see cref="CommandManager"/>.
+        /// </summary>
+        public void NotifyCanExecuteChanged()
+        {
+            // do nothing
+        }
+    }
+}
