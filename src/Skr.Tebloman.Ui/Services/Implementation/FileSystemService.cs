f10da794174b805c1b28db388e0980ad0698a534
skr444 <15791510+skr444@users.noreply.github.com>
2025-08-07 12:00:35 +0200

    feat: abstract file system

---
+++ src/Skr.Tebloman.Ui/Services/Implementation/FileSystemService.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Diagnostics;
+using Skr.Tebloman.Infrastructure.Storage.Api;
+
+namespace Skr.Tebloman.Ui.Services.Implementation
+{
+    /// <inheritdoc/>
+    internal sealed class FileSystemService : IFileSystemService
+    {
+        private const string ExplorerExe = "explorer.exe";
+
+        private readonly IFileStorage storage;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="FileSystemService"/>.
+        /// </summary>
+        /// <param name="fileStorage">File storage service.</param>
+        public FileSystemService(IFileStorage fileStorage)
+        {
+            storage = fileStorage;
+        }
+
+        /// <inheritdoc/>
+        public void OpenApplicationDataFolderInExplorer()
+        {
+            OpenFolderInExplorer(storage.StorageDirectory);
+        }
+
+        /// <inheritdoc/>
+        public void OpenFolderInExplorer(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Process.Start(ExplorerExe, path);
+            }
+        }
+    }
+}
