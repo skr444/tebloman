dd436407c6a6cd26495f3f63f87922c5fdaca7ce
skr444 <15791510+skr444@users.noreply.github.com>
2025-08-07 11:35:35 +0200

    feat: add runtime component
    Created a component for central cancellation token management.

---
+++ src/Skr.Tebloman.Infrastructure.Runtime/Implementation/LifecycleManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+using Skr.Tebloman.Infrastructure.Runtime.Api;
+
+namespace Skr.Tebloman.Infrastructure.Runtime.Implementation
+{
+    /// <inheritdoc />
+    internal sealed class LifecycleManager : ILifecycleManager
+    {
+        private readonly CancellationTokenSource cts;
+        private readonly Action shutdownSequence;
+
+        /// <inheritdoc />
+        public CancellationToken Token => cts.Token;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="LifecycleManager"/>.
+        /// </summary>
+        /// <param name="shutdown">The shutdown sequence.</param>
+        public LifecycleManager(Action shutdown)
+        {
+            cts = new CancellationTokenSource();
+            shutdownSequence = shutdown;
+        }
+
+        /// <inheritdoc />
+        public void Cancel()
+        {
+            cts.Cancel();
+        }
+
+        /// <inheritdoc />
+        public void Shutdown()
+        {
+            cts.Cancel();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            cts.Dispose();
+            shutdownSequence?.Invoke();
+        }
+    }
+}
